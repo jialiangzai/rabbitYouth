caa42ee528e08a5f5713ee5bffc4636f1154cddd
zuishuainanhai <1705902650@qq.com>
2021-11-30 18:54:42 +0800

    二次封装

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
     测试第三方插件实现vuex持久化
   </button> -->
 </template>
-
+<script>
+// 测试 import request from '@/utils/request.js'
+export default {
+  setup () {
+    // request('/home/banner', 'get').then(res => console.log(res))
+  }
+}
+</script>
 <style lang="less">
 </style>
