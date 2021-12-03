ee13031ffaed2477eb1b5499df97253a625fde52
zuishuainanhai <1705902650@qq.com>
2021-12-03 20:12:06 +0800

    vue3实现吸顶

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,8 @@
   <TopNav />
   <!-- 头部 -->
   <XtxHeader />
+  <!-- 吸顶 -->
+  <XtxHeaderSticky />
   <main>
     <!-- 二级路由出口 -->
     <router-view></router-view>
@@ -15,12 +17,14 @@
 import TopNav from './components/topnav.vue'
 import XtxHeader from './components/header.vue'
 import XtxFooter from './components/footer.vue'
+import XtxHeaderSticky from './components/header-sticky.vue'
 export default {
   name: 'xtx-layout',
   components: {
     TopNav,
     XtxHeader,
-    XtxFooter
+    XtxFooter,
+    XtxHeaderSticky
   }
 }
 </script>
