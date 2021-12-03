3fb6795de259588883bbd37669598e9907e21483
zuishuainanhai <1705902650@qq.com>
2021-12-03 19:11:56 +0800

    less样式处理

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 重置样式
+import 'normalize.css'
+// 公共样式
+import './styles/common.less'
 createApp(App).use(store).use(router).mount('#app')
