b3f9c6c08ba3dadeef3d58bddf6be38df24fb0cc
zuishuainanhai <1705902650@qq.com>
2021-12-07 02:09:10 +0800

    面包屑导航栏渲染

--- src/views/Goods/index.vue
+++ src/views/Goods/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="xtx-goods-page">
+  <div class="xtx-goods-page" v-if="goods.categories">
     <div class="container">
       <!-- 面包屑 -->
       <XtxBread separator="/">
+        <!-- 首页固定 -->
         <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem to="/">手机</XtxBreadItem>
-        <XtxBreadItem to="/">华为</XtxBreadItem>
-        <XtxBreadItem to="/">p30</XtxBreadItem>
+        <!-- 一级 -->
+        <XtxBreadItem to="/">{{ goods.categories[1].name }}</XtxBreadItem>
+        <!-- 二级 -->
+        <XtxBreadItem to="/">{{ goods.categories[0].name }}</XtxBreadItem>
+        <!-- 自身 -->
+        <XtxBreadItem to="/">{{ goods.name }}</XtxBreadItem>
       </XtxBread>
       <!-- 商品信息 -->
       <div class="goods-info"></div>
@@ -25,8 +29,26 @@
 </template>
 
 <script>
+import { findGoods } from '@/api/goods'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue-demi'
+
 export default {
-  name: 'XtxGoodsPage'
+  name: 'XtxGoodsPage',
+  setup () {
+    const goods = ref({})
+    const route = useRoute()
+    const goodsDetail = async () => {
+      const { result } = await findGoods(route.params.id)
+      console.log('当前商品信息', result)
+      goods.value = result
+    }
+    goodsDetail()
+    return {
+      goods
+
+    }
+  }
 }
 </script>
 
